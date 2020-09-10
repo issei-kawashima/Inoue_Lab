9e2202b2880e6837739e2859282e30c7c712912b
Issei_kawashima <isseikawashima11@gmail.com>
2020-09-10 13:22:10 +0900

    等値面の可視化ができた！

--- analysis_tools/Integrate_Invaritant2.f90
+++ analysis_tools/Integrate_Invaritant2.f90
@@ -1,14 +1,16 @@
 !3次元結果の第二不変量Qのみを複数z面から1つのtxtファイルにまとめるコード
 !2次元txtを1つのtxtにまとめる機能も兼ねている
 !NaNの時でも対応できるように、読み込む変数の数を調整したコードも書いてある(コメントアウト済み)
-!2020.08.26 Q=xxxの特定面のみの情報を切り出す処理は未実装
+!等値面をparaviewで可視化したいときは、particlesファイルで出力する。
+!正直等値面表示以外もparticlesでできるので、txtファイルへの出力する機能はいらないかもしない
+
 program main
 implicit none
 integer i,j,k,l,time
 integer,parameter :: Nx = 360+1 !一行改行分を含めてる
 integer,parameter :: Ny = 200
 integer,parameter :: Nz = 20
-integer,parameter :: CNT = 0
+integer,parameter :: CNT = 256
 double precision,allocatable,dimension(:,:,:,:) :: dim_kukei
 character(len = 16) z_name
 character(len = 16) folder_name
@@ -37,27 +39,42 @@ enddo
 !複数の2次元txtファイルから第二不変量と座標のみを1つのtxtファイルにまとめる時はこちら
 ! open(99, file = "/Users/isseyshome/Documents/GitHub/Inoue_Lab/3dimensions/result_analysis/"&
 !           //trim(folder_name)//"_2ndInvaritant"//trim(M_number)//".txt")
-!複数の2次元txtファイルを1つのtxtファイルにまとめる時はこちら
+!paraviewでの等値面可視化用ファイル
+!複数の2次元txtファイルから第二不変量と座標のみを1つのparticlesファイルにまとめる時はこちら
+! open(99, file = "/Users/isseyshome/Documents/GitHub/Inoue_Lab/3dimensions/result_analysis/"&
+!           //trim(folder_name)//"_2ndInvaritant"//trim(M_number)//".particles")
 open(99, file = "/Users/isseyshome/Documents/GitHub/Inoue_Lab/3dimensions/result_analysis/"&
-          //trim(folder_name)//"_All"//trim(M_number)//".txt")
+          //trim(folder_name)//"_div_u"//trim(M_number)//".particles")
+!複数の2次元txtファイルを1つのtxtファイルにまとめる時はこちら
+! open(99, file = "/Users/isseyshome/Documents/GitHub/Inoue_Lab/3dimensions/result_analysis/"&
+!           //trim(folder_name)//"_All"//trim(M_number)//".txt")
 do k = 0,Nz-1
   do i =0,Ny
     do j =0,Nx
       if((dim_kukei(j,i,k,1) == 0.d0) .and. (dim_kukei(j,i,k,3) == 0.d0) .and.&
        (dim_kukei(j,i,k,5) == 0.d0) .and. (dim_kukei(j,i,k,7) == 0.d0)) then
       else
-!(通常&NaN直前)第二不変量のみの時はこちら
+!(通常&NaN直前)第二不変量のみの時のtxtファイル出力はこちら
         ! write(99,'(f24.16,",",f24.16,",",f24.16,",",f24.16)') &
         ! dim_kukei(j,i,k,1),dim_kukei(j,i,k,3),dim_kukei(j,i,k,5),&
-        ! dim_kukei(j,i,k,9)!x,y,z,第二不変量Q(~2020.08.26)
+        ! ! dim_kukei(j,i,k,9)!x,y,z,第二不変量Q(~2020.08.26)
         ! dim_kukei(j,i,k,11)!x,y,z,第二不変量Q(2020.08.27~出力順を変更した)
+!(通常&NaN直前)第二不変量のみの時のparticlesファイル出力はこちら
+        ! write(99,'(4f24.16)') &
+        ! dim_kukei(j,i,k,1),dim_kukei(j,i,k,3),dim_kukei(j,i,k,5),&
+        ! ! dim_kukei(j,i,k,9)!x,y,z,第二不変量Q(~2020.08.26)
+        ! dim_kukei(j,i,k,11)!x,y,z,第二不変量Q(2020.08.27~出力順を変更した)
+!(通常&NaN直前)第二不変量のみの時のparticlesファイル出力はこちら
+        write(99,'(4f24.16)') &
+        dim_kukei(j,i,k,1),dim_kukei(j,i,k,3),dim_kukei(j,i,k,5),&
+        dim_kukei(j,i,k,9)!x,y,z,div_u
 
 !(通常)全ての変数を1つのtxtファイルにまとめる時はこちら
-        write(99,'(f24.16,",",f24.16,",",f24.16,",",f24.16,",",f24.16,&
-        ",",f24.16,",",f24.16)') &
-        dim_kukei(j,i,k,1),dim_kukei(j,i,k,3),dim_kukei(j,i,k,5),&
-        dim_kukei(j,i,k,7),dim_kukei(j,i,k,9),dim_kukei(j,i,k,11),&
-        dim_kukei(j,i,k,13)
+        ! write(99,'(f24.16,",",f24.16,",",f24.16,",",f24.16,",",f24.16,&
+        ! ",",f24.16,",",f24.16)') &
+        ! dim_kukei(j,i,k,1),dim_kukei(j,i,k,3),dim_kukei(j,i,k,5),&
+        ! dim_kukei(j,i,k,7),dim_kukei(j,i,k,9),dim_kukei(j,i,k,11),&
+        ! dim_kukei(j,i,k,13)
 
 !(NaN直前)全ての変数を1つのtxtファイルにまとめる時はこちら
         ! write(99,'(f24.16,",",f24.16,",",f24.16,",",f24.16,",",f24.16,&
